d2f5991465ecbb2ab699d4a7d288e936af48ed17
Daniel Mątwicki <jkeezuz@gmail.com>
2018-08-16 20:07:24 +0100

    Slighty improved graph

--- graph_maker.py
+++ graph_maker.py
@@ -1,5 +1,5 @@
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, ColumnDataSource
 
 
 class GraphMaker:
@@ -9,12 +9,22 @@ class GraphMaker:
 
     @staticmethod
     def generateQuadGraphDatetime(dataframe):
+        dataframe["Start_string"] = dataframe["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+        dataframe["End_string"] = dataframe["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+        cds=ColumnDataSource(dataframe)
+
         p = figure(x_axis_type = 'datetime', height = 500, width = 1000, title = "Motion graph")
+
+        hover = HoverTool(tooltips=[("Start", "@Start_string"), ("End", "@End_string")])
+        p.add_tools(hover)
+
+
         p.yaxis.minor_tick_line_color = None
         p.ygrid[0].ticker.desired_num_ticks=1
 
 
-        q = p.quad(left=dataframe["Start"], right=dataframe["End"], bottom=0, top=1, color="green")
+        q = p.quad(left="Start", right="End", bottom=0, top=1, color="green", source=cds)
 
         output_file("Graph.hmtl")
 
